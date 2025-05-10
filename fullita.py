96287c351590d07fc2d96e4e2ff73ad6946655a4
DanielsJD <daniele.loliva@gmail.com>
2025-05-10 13:23:27 +0200

    Update fullita.py

--- fullita.py
+++ fullita.py
@@ -12,8 +12,8 @@ import time
 
 # Constants
 
-LINKMFP = "https://tuonome.hf.space"
-PASSWORD = "tuapassword"
+LINKMFP = "https://danielsjd-mickeyomg.hf.space"
+PASSWORD = "DaX9091"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 
