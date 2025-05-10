6f5977a8f2f6974d54c975bb0397596b85877ca1
DanielsJD <daniele.loliva@gmail.com>
2025-05-10 13:23:46 +0200

    Update itaevents.py

--- itaevents.py
+++ itaevents.py
@@ -13,8 +13,8 @@ import re
 from urllib.parse import quote_plus  # Add this import
 
 # Constants
-LINKMFP = "https://tuonome.hf.space"
-PASSWORD = "tuapassword"
+LINKMFP = "https://danielsjd-mickeyomg.hf.space"
+PASSWORD = "DaX9091"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 
